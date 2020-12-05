135e1a150ca24ea62030884f75c951dba31be231
q2aprick <74082290+q2aprick@users.noreply.github.com>
2020-12-05 17:15:52 +0700

    Add files via upload

---
+++ qa-userid-display-overrides.php
@@ -0,0 +1,27 @@
+<?php
+
+  	// override for qa-app-users.php qa_get_one_user_html() function
+	//
+	function qa_get_one_user_html($handle, $microdata = false, $favorited = false)
+	{
+		if (strlen($handle) === 0) {
+			return qa_lang('main/anonymous');
+		}
+
+		$url = qa_path_html('user/' . $handle);
+		$favclass = $favorited ? ' qa-user-favorited' : '';
+		$userprofiles = qa_db_select_with_pending(qa_db_user_profile_selectspec($handle,false));
+		if (isset($userprofiles['name'])) {
+		  		  $fullname =   $userprofiles['name'];
+		$userHtml =   	$microdata ? '<span itemprop="author" itemscope itemtype="https://schema.org/Person"><a href="' . $url . '" class="qa-user-link' . $favclass . '" itemprop="url"> <span itemprop="name">' . qa_html($fullname) . '</span></a> @'.qa_html($handle).'</span>': qa_html($fullname).' @'.qa_html($handle) ;
+		}
+       else {
+        $userHtml =   	$microdata ? '<span itemprop="author" itemscope itemtype="https://schema.org/Person"><a href="' . $url . '" class="qa-user-link' . $favclass . '" itemprop="url"> <span itemprop="name">' . qa_html($handle) . '</span></a></span>': qa_html($handle); 
+       }
+
+		return $userHtml;
+	}
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
