dd0ee9496a8406ff58088293db42a9bc011279ef
q2aprick <74082290+q2aprick@users.noreply.github.com>
2020-12-05 22:48:16 +0700

    Update fullname.php

--- fullname.php
+++ fullname.php
@@ -10,7 +10,7 @@
 		$url = qa_path_html('user/' . $handle);
 		$favclass = $favorited ? ' qa-user-favorited' : '';
 		$userprofiles = qa_db_select_with_pending(qa_db_user_profile_selectspec($handle,false));
-		if (isset($userprofiles['name'])) {
+		if (strlen($userprofiles['name']) > 0) {
 		  		  $fullname =   $userprofiles['name'];
 		$userHtml =   	$microdata ? '<span itemprop="author" itemscope itemtype="https://schema.org/Person"><a href="' . $url . '" class="qa-user-link' . $favclass . '" itemprop="url"> <span itemprop="name">' . qa_html($fullname) . '</span></a> @'.qa_html($handle).'</span>': qa_html($fullname).' @'.qa_html($handle) ;
 		}
