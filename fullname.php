1687ea8c76d1889b542aab40278c9e6bd4c23cfc
q2aprick <74082290+q2aprick@users.noreply.github.com>
2020-12-22 15:06:26 +0700

    Update fullname.php

--- fullname.php
+++ fullname.php
@@ -12,10 +12,10 @@
 		$userprofiles = qa_db_select_with_pending(qa_db_user_profile_selectspec($handle,false));
 		if (strlen($userprofiles['name']) > 0) {
 		  		  $fullname =   $userprofiles['name'];
-		$userHtml = $microdata ? '<span itemprop="author" itemscope itemtype="https://schema.org/Person"><a href="' . $url . '" class="qa-user-link' . $favclass . '" itemprop="url"> <span style="font-weight: bolder;" itemprop="name">' . qa_html($fullname) . '</span></a> @'.qa_html($handle).'</span>': '<span style="font-weight: bolder;">'.qa_html($fullname).'</span> @'.qa_html($handle) ;
+		$userHtml =   	$microdata ? '<span itemprop="author" itemscope itemtype="https://schema.org/Person"><a href="' . $url . '" class="qa-user-link' . $favclass . '" itemprop="url" rel="nofollow"> <span style="font-weight: bolder;" itemprop="name">' . qa_html($fullname) . '</span></a> @'.qa_html($handle).'</span>': '<a href="' . $url . '" class="qa-user-link' . $favclass . '" rel="nofollow"><span style="font-weight: bolder;">'. qa_html($fullname).'</span></a> @'.qa_html($handle) ;
 		}
-                else {
-                $userHtml = $microdata ? '<span itemprop="author" itemscope itemtype="https://schema.org/Person"><a href="' . $url . '" class="qa-user-link' . $favclass . '" itemprop="url"> <span itemprop="name">' . qa_html($handle) . '</span></a></span>': qa_html($handle); 
+       else {
+        $userHtml =   	$microdata ? '<span itemprop="author" itemscope itemtype="https://schema.org/Person"><a href="' . $url . '" class="qa-user-link' . $favclass . '" itemprop="url" rel="nofollow"> <span itemprop="name">' . qa_html($handle) . '</span></a></span>': '<a href="' . $url . '" class="qa-user-link' . $favclass . '" rel="nofollow">'.qa_html($handle).'</a>'; 
                 }
 
 		return $userHtml;
